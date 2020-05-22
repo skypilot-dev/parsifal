d76c22a20bbe2d3cf265638c41821a3f371c1a1f
William Thorsen <william@thorsen.dev>
2020-05-22 09:17:36 -0600

    refactor: Move constrained-arg validation to separate file
    Moved `validateConstrainedArgs` out of the `validateNamedArgs` file

--- src/parseCliArgs/validateNamedArgs.ts
+++ src/parseCliArgs/validateNamedArgs.ts
@@ -1,20 +1,6 @@
-import { getOrDefault } from 'src/lib/functions/object/getOrDefault';
 import { ArgumentsMap, NamedArgumentDef, ValidationException } from './_types';
-import { validateConstrainedValue } from './validators/validateConstrainedValue';
+import { validateConstrainedArgs } from './validators/validateConstrainedArgs';
 import { validateRequiredArgs } from './validators/validateRequiredArgs';
-
-function validateConstrainedArgs(
-  argsMap: ArgumentsMap, argDefs: NamedArgumentDef[]
-): ValidationException[] {
-  return argDefs.reduce((accExceptions, argDef) => {
-    const { defaultValue, name } = argDef;
-    const value = getOrDefault(argsMap, name, defaultValue);
-    return [
-      ...accExceptions,
-      ...validateConstrainedValue(value, argDef),
-    ];
-  }, [] as ValidationException[]);
-}
 
 export function validateNamedArgs(
   argsMap: ArgumentsMap, namedArgDefs: NamedArgumentDef[]
