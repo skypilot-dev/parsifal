ea05897d0630cd24e1b5162ab4b7979a0e52f993
William Thorsen <william@thorsen.dev>
2020-05-24 13:27:59 -0600

    feat: Show improved positional-arg value labels in usage

--- src/parseCliArgs/formatters/formatPositionalArgsUse.ts
+++ src/parseCliArgs/formatters/formatPositionalArgsUse.ts
@@ -4,23 +4,5 @@ import { formatValueLabel } from './formatValueLabel';
 /* FIXME: Add tests */
 
 export function formatPositionalArgsUse(argDefs: ArgumentDefinition[]): string {
-  const requiredArgDefs = argDefs.filter(({ required }) => !!required);
-  const optionalArgDefs = argDefs.filter(({ required }) => !required);
-
-  const requiredArgUsage = requiredArgDefs.length
-    ? requiredArgDefs.map(({ name }) => `<${name}>`).join(' ')
-    : '';
-
-  const optionalArgUsage = optionalArgDefs.length
-    ? `[${optionalArgDefs.map((argDef) => formatValueLabel(argDef)).join(' ')}]`
-    : '';
-
-  const argUsage: string[] = [];
-  if (requiredArgUsage) {
-    argUsage.push(requiredArgUsage);
-  }
-  if (optionalArgUsage) {
-    argUsage.push(optionalArgUsage);
-  }
-  return argUsage.join(' ');
+  return argDefs.map(argDef => formatValueLabel(argDef)).join(' ');
 }
