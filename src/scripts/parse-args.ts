0b1bc156e2fd1ed880a527d9278aff628a68b359
William Thorsen <william@thorsen.dev>
2020-06-09 08:09:39 -0600

    tooling: Upgrade all dependencies

--- src/scripts/parse-args.ts
+++ src/scripts/parse-args.ts
@@ -1,5 +1,12 @@
 import { parseCliArgs } from '../parseCliArgs';
 
-const parsedArgs = parseCliArgs();
+const parsedArgs = parseCliArgs({
+  named: [
+    { name: 'myNamedArg', aliases:  ['n'] },
+  ],
+  positional: [
+    { name: 'positional' },
+  ],
+});
 
 console.log('parsedArgs:', parsedArgs)
